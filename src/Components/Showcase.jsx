cac5c830557ecf5a86d3d661ba769b6cc54fd1f1
Ugiloy Toshmirzaeva <142172764+UgiloyToshmirzaeva@users.noreply.github.com>
2023-11-03 02:48:25 +0500

    Add files via upload

---
+++ src/Components/Showcase.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import Button from './Btn';
+import Title from './Title';
+import Card from './Card'
+
+const Showcase = () => {
+    return (
+        <div className="showcase">
+            
+        </div>
+    );
+};
+
+export default Showcase;
