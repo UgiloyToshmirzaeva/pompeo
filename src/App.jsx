515bb876e2d53b150847502c83563022aad2bd37
ugiloitoshmirzaeva <ugiloitoshmirzaeva@gmail.com>
2023-11-03 09:29:37 +0500

    Component styles are added and changed some .jsx files

---
+++ src/App.jsx
@@ -0,0 +1,27 @@
+import './App.css'
+import Nav from './Components/Nav'
+import Showcase from './Components/Showcase'
+// import Categories from './Components/Categories'
+import Collection from './Components/Collection'
+// import Shop from './Components/Shop' // 
+// import News from './Components/News' 
+// import Footer from './Components/Footer'
+
+function App() {
+  return (
+    <div className='Pompeo'>
+      <Nav></Nav>
+      {/* <Nav></Nav>
+      <Showcase></Showcase>
+      <Categories></Categories>
+      <Collection></Collection>
+      <Shop></Shop>
+      <News></News> 
+      <Footer></Footer> */}
+      <Showcase></Showcase>
+      <Collection></Collection>
+    </div>
+  )
+}
+
+export default App
